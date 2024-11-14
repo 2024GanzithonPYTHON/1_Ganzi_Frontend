9c38daaf5ec35867d36f67713f1e2fb7b9a97a16
hahee <hahee_hi@naver.com>
2024-11-14 17:01:59 +0900

    처음

---
+++ src/styles/StyledLogin.jsx
@@ -0,0 +1,23 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 393px;
+  height: 852px;
+  margin-top: 0px;
+  background: #fff;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  min-height: 100vh;
+  overflow-y: hidden;
+  overflow-x: hidden;
+`;
+
+export const Logo = styled.div`
+  width: 172.471px;
+  height: 79px;
+  flex-shrink: 0;
+  margin-top: 59px;
+`;
